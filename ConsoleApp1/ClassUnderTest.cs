8d39851c3fb2df58a9e8af33330188091a7195ca
Jason Tennant <xcodula@icloud.com>
2017-05-02 08:05:46 -0700

    init

---
+++ ConsoleApp1/ClassUnderTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class ClassUnderTest
+    {
+        public int GoodAddIntegers(int x, int y)
+        {
+            return x + y; // good
+        }
+        public int BadAddIntegers(int x, int y)
+        {
+            return x * y; // bad
+        }
+    }
+}
---
+++ UnitTestProject/UnitTests.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestGoodAddIntegers()
+        {
+            // Arrange
+            ClassUnderTest cut = new ClassUnderTest();
+
+            // Act
+            int result = cut.GoodAddIntegers(3, 4); // passes
+
+            // Assert
+            Assert.AreEqual(result, 3 + 4);
+        }
+        [TestMethod]
+        public void TestBadAddIntegers()
+        {
+            // Arrange
+            ClassUnderTest cut = new ClassUnderTest();
+
+            // Act
+            int result = cut.BadAddIntegers(3, 4); // fails
+
+            // Assert
+            Assert.AreEqual(result, 3 + 4);
+        }
+    }
+}
